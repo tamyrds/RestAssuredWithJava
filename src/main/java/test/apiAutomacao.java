4817c927e592f283221140abdb27ed087f3f11c0
Tamires Rodrigues <tamy.rdasilva@gmail.com>
2023-07-09 22:14:29 -0300

    Realizado criação de teste de 3 endpoints/

--- src/main/java/test/apiAutomacao.java
+++ src/main/java/test/apiAutomacao.java
@@ -3,23 +3,30 @@ package test;
 import io.restassured.RestAssured;
 import netscape.javascript.JSObject;
 import org.hamcrest.Matchers;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.net.URI;
 
+import static io.restassured.RestAssured.given;
+
 public class apiAutomacao {
 
+    @BeforeClass
+    public static void setup(){
+        RestAssured.baseURI= "https://serverest.dev";
+    }
     @Test
     public void CadastrarUser() {
 
-        RestAssured.given()
+        given()
                 .log().all()
                 .contentType("application/json")
                 .body("{\n" +
-                        "    \"nome\":\"Tamires Silva\", \"email\":\"teste@te.com.br\",\"password\":\"teste123\",\"administrador\": \"true\"\n" +
+                        "    \"nome\":\"Tamires Silva\", \"email\":\"teste@te1234.com.br\",\"password\":\"teste123\",\"administrador\": \"true\"\n" +
                         "        }")
                 .when()
-                .post("https://serverest.dev/usuarios")
+                .post("/usuarios")
                 .then()
                 .log().all()
                 .statusCode(201)
@@ -29,25 +36,25 @@ public class apiAutomacao {
 
     @Test
     public void listasUsuarios() {
-        RestAssured.given()
+        given()
                 .contentType("application/json")
                 .when()
-                .get("https://serverest.dev/usuarios")
+                .get("/usuarios")
                 .then()
 
                 .statusCode(200)
-                .body("usuarios.nome[0]", Matchers.is("Fulano da Silva"))
+                .body("usuarios.nome[0]", Matchers.is("Sarah Green III"))
         ;
     }
 
     @Test
     public void realizarLoginSucesso() {
-        RestAssured.given()
+        given()
                 .log().all()
                 .contentType("application/json")
                 .body("{\"email\": \"beltrano@qa.com.br\", \"password\": \"teste\"}")
                 .when()
-                .post("https://serverest.dev/login")
+                .post("/login")
                 .then()
                 .statusCode(200)
                 .log().all()
@@ -57,15 +64,59 @@ public class apiAutomacao {
 
     @Test
     public void realizarLoginInvalido() {
-        RestAssured.given()
+        given()
                 .contentType("application/json")
                 .body("{\"email\": \"beltran@qa.com.br\", \"password\": \"teste\"}")
                 .when()
-                .post("https://serverest.dev/login")
+                .post("/login")
                 .then()
                 .statusCode(401)
                 .log().all()
                 .body("message", Matchers.is("Email e/ou senha inválidos"))
+        ;
+    }
+    @Test
+    public void buscarUsuariosPeloId(){
+        given()
+                .when()
+                .get("/usuarios/FQsZWahMJNqxDntd")
+                .then()
+                .statusCode(200)
+                .log().all()
+                .body("email", Matchers.is("alida.schoen@hotmail.com"))
+                ;
+    }
+    @Test
+    public void buscarUsuarioInvalidoPeloId(){
+        given()
+                .when()
+                .get("/usuarios/123455")
+                .then()
+                .statusCode(400)
+                .log().all()
+                .body("message", Matchers.is("Usuário não encontrado"));
+
+    }
+    @Test
+    public void deveRemoverUsuario(){
+        given()
+                .when()
+                .delete("/usuarios/1yUinoOT5p4TFJiu")
+                .then()
+                .statusCode(200)
+                .log().all()
+                .body("message", Matchers.is("Registro excluído com sucesso"))
+                ;
+    }
+    @Test
+    public void deveRemoverUsuarioJaRemovido(){
+        given()
+                .when()
+                .delete("/usuarios/1yUinoOT5p4TFJiu")
+                .then()
+                .statusCode(200)
+                .log().all()
+                .body("message", Matchers.is("Nenhum registro excluído"))
         ;
     }
 }
